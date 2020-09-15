1f979e9b3f508fba9305652dc30b8574d9dba4c2
Md. Ariful Islam <arifjmamun24@gmail.com>
2020-09-16 05:11:47 +0600

    Added missing files and docker-compose.yml file

---
+++ Producer/Abstractions/IProducer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Producer.Abstractions
+{
+    public interface IProducer
+    {
+        /// <summary>
+        /// Produce message on given topic
+        /// </summary>
+        /// <param name="topic"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        Task<string> PublishAsync(string topic, string message);
+    }
+}
